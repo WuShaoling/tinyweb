d64cf92b4f985e89cfa2a94935b643f8c91480a0
WSL <wu12490@gmail.com>
2019-04-12 11:32:33 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/tinyweb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyweb.dir/main.c.o"
+  "tinyweb.pdb"
+  "tinyweb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinyweb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
